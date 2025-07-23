f7bcfaf4f9fed66302904376f8ef9376fc92a6ea
Awomiah <priscillamensah4000@gmail.com>
2025-07-23 12:58:03 +0000

    update

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const removeBtn = document.createElement('button');
             removeBtn.textContent = "Remove";
-            removeBtn.className = 'remove-btn';
+            removeBtn.classList.add('remove-btn'); // <-- REQUIRED METHOD
 
             removeBtn.onclick = function () {
                 taskList.removeChild(li);
